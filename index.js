13cc90cbecd0628e89ddeea556b72fffa446583f
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-07-27 13:46:23 +0200

    push push

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ app.use(bodyParser.json())
 /**
  * WEB PUSH
  */
-/* webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, 
+webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, 
     process.env.PUBLIC_VAPID_KEY, 
-    process.env.PRIVATE_VAPID_KEY); */
+    process.env.PRIVATE_VAPID_KEY);
 
 /**
  * GET
